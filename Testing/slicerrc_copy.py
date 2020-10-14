c60ac187abc3143ef9360466e618dccf72128563
James Cook <jamesjcook@noreplay.github.com>
2020-10-14 17:24:34 -0400

    Updates on the path to generalization into __init__

--- Testing/slicerrc_copy.py
+++ Testing/slicerrc_copy.py
@@ -6,22 +6,45 @@ import os
 import re
 import math
 import sys
+import qt
+import slicer
 
+
+#import inspect
+#from pathlib import Path
+
+# import without destroying __file__
+#   import sys;sys.path.append(r"h:\code\ndLibraryViewer");sys.path.append(r"h:\code\ndLibraryViewer\Testing");import slicerrc_copy
 ## Specify the path to the code and the path to the root ndLibrary in question
-codePath = "D:\CIVM_Apps\Slicer\\ndLibraryViewer"
+#codePath = r"h:\code\ndLibraryViewer"
+
+#codePath = os.path.join(os.path.dirname(os.path.abspath(__file__)),'..')
+codePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 #rootDir = r"D:\Libraries\040Human_Brainstem"
 rootDir = r"D:\Libraries\010Rat_Brain\v2020-06-25"
 
 ## Find code and execute it
-notPython3 = '2.7' in sys.version
+isPy2 = '2.7' in sys.version
+print("setup code from "+codePath)
 for file in os.listdir(codePath):
     if os.path.isdir(os.path.join(codePath, file)):
         continue
-    if notPython3 and 'ndLibrary_Python3.py' in file:
+    if isPy2 and 'ndLibrary_Python3.py' in file:
         continue
-    if not notPython3 and 'ndLibrary.py' in file:
+    if not isPy2 and 'ndLibrary.py' in file:
         continue
-    exec(open(os.path.join(codePath, file)).read())
+    try:
+    #if 1 :
+        #import importlib.util
+        #spec = importlib.util.spec_from_file_location("module.name", "/path/to/file.py")
+        #spec = importlib.util.spec_from_file_location(file, os.path.join(codePath, file))
+        #foo = importlib.util.module_from_spec(spec)
+        #spec.loader.exec_module(foo)
+        exec(open(os.path.join(codePath, file)).read())
+    except:
+        print("Couldn't open "+file+" from "+codePath)
+        print("exec(open(r\""+os.path.join(codePath, file)+"\").read())")
 
 ## Initialize the ndLibrary viewer
 initializeNDLibraryViewer(rootDir)
