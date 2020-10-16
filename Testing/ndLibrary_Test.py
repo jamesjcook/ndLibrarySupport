98c566dbe2a654a16cd89c58ce875d924379f3d7
James Cook <jamesjcook@noreplay.github.com>
2020-10-16 17:39:21 -0400

    added more extensive testing bits

--- Testing/ndLibrary_Test.py
+++ Testing/ndLibrary_Test.py
@@ -1,10 +1,26 @@
-## To run, type: execfile("D:\CIVM_Apps\Slicer\FiberCompareView\\2D_Atlas\\ndLibrary_Test.py")
+
+## To run, type: exec(open(r"h:\code\ndLibrarySupport\Testing\ndLibrary_Test.py").read())
 ## Author: Austin Kao
-execfile("D:\CIVM_Apps\Slicer\FiberCompareView\\2D_Atlas\\ndLibrary.py")
-#f = ndLibrary(None, r"D:\Libraries\010Rat_Brain")
-#f = ndLibrary(None, r"D:\CIVM_Apps\Slicer\FiberCompareViewTestData\\090_Heritability")
+exec(open(r"h:\code\ndLibrarySupport\ndLibrary.py").read())
+f = ndLibrary(None, r"D:\Libraries\010Rat_Brain")
+f.children[0].conf_dir
+lib=f.children[0]
+len(lib.children)
+
+lib=lib.children[0]
+lib.volDict
+for vol in lib.volDict:
+    print vol
+filter=lib.fields["FilePattern"]
+pattern=lib.fields["FileAbrevPattern"]
+match=lib.fields["FileAbrevMatch"]
+lib.jumpToDir()
+libEntries = [f for f in os.listdir(os.getcwd()) if re.match(r''+filter, f) and not os.path.isdir(os.path.join(os.getcwd(),f))]
+
+f = ndLibrary(None, r"D:\Libraries\010Rat_Brain\v2020-10-16\21Rat")
+#f = ndLibrary(None, r"D:\Libraries\010Rat_Brain\v2020-10-16\23Rat")
 #f = ndLibrary(None, r"D:\Libraries\010Rat_Brain\v2020-06-25\23Rat")
-f = ndLibrary(None, r"D:\Libraries\010Rat_Brain\v2020-06-25\21Rat")
+#f = ndLibrary(None, r"D:\CIVM_Apps\Slicer\FiberCompareViewTestData\\090_Heritability")
 #print("Testing loading of a conf file")
 #f.loadConf(r"D:\Libraries\010Rat_Brain\lib.conf")
 #print("Testing building children")
