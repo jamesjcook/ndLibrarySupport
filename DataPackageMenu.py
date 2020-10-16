233bc66005f67f560859047801af1b6b8eb6e657
James Cook <jamesjcook@noreplay.github.com>
2020-10-16 17:32:31 -0400

    fixed instanced function for qmenu

--- DataPackageMenu.py
+++ DataPackageMenu.py
@@ -4,6 +4,7 @@
 class DataPackageMenu(qt.QMenu):
     ## ndLibraries is the list of ndLibraries to be displayed in the data package menu
     ## Places menu in the top menu bar (With File, Edit, etc.)
+    libDict = dict()
     def __init__(self, ndLibraries):
         if not isinstance(ndLibraries, list):
             print("Not a list")
@@ -13,8 +14,6 @@ class DataPackageMenu(qt.QMenu):
         mainMenuBar = mainWindow.findChild("QMenuBar", "menubar")
         mainMenuBar.addMenu(self)
         self.title = "Data Packages"
-        for toolbar in mainWindow.findChildren("QToolBar"):
-            toolbar.setVisible(0)
         ## Instantiate the controller of the ndLibraryViewer
         self.controller = AtlasController()
         ## Create the items in the menu and attach functions to each one
@@ -29,6 +28,8 @@ class DataPackageMenu(qt.QMenu):
             menuItem = qt.QAction(name, self)
             self.addAction(menuItem)
             ## Define a function for the menu item to execute when selected
-            def loadRelevantLibrary():
-                self.controller.setUpLibrary(lib)
-            menuItem.triggered.connect(loadRelevantLibrary)
+            ## MUST must be a lambda for this to work
+            ## storing our lambda's for later so we can manually trigger them.
+            self.libDict[name] = lambda instancedLoader, lib=lib: self.controller.setUpLibrary(lib)
+            menuItem.triggered.connect(self.libDict[name])
+            #menuItem.triggered.connect(self.setUpLib)
