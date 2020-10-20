9ff8901ab5c19f9fbe9da6ddac5b247c53a222e0
James Cook <jamesjcook@noreplay.github.com>
2020-10-20 17:29:38 -0400

    Finished basic functionality
    re-worked varnames to more common standard

--- conf.py
+++ conf.py
@@ -1,7 +1,9 @@
 ## lib.conf loader/saver/handler 
+import os
+import re
 
 class conf:
-    def __init__(self,confPath):
+    def __init__(self,conf_path,conf_name="lib.conf"):
         # fields is the name ready to use fields
         self.fields=dict()
         # comments will be line num : comment
@@ -9,10 +11,20 @@ class conf:
         # lines will be line num : field key
         self.lines=dict()
         self.lineCount = 0
-        if os.path.isfile(confPath):
-            #self.conf_path = conf_path
-            #print(self.conf_dir)
-            self.load(confPath)
+        # in the future perhaps we support "json" mode?
+        self.mode="conf"
+        if os.path.isfile(conf_path):
+            self.conf_path=conf_path
+            self.conf_dir=os.path.dirname(conf_path)
+            self.conf_name=os.path.basename(conf_path)
+        else:# os.path.isdir(conf_path):
+            self.conf_path=os.path.join(conf_path,conf_name)
+            self.conf_dir=conf_path
+            self.conf_name=conf_name
+        if os.path.isfile(self.conf_path):
+            self.load(self.conf_path)
+        else:
+            print("New empty conf for "+self.conf_path)
     ## Method to load a lib.conf file for a ndLibrary and store the name-value pairs it contains
     def load(self, file=None):
         if file is None:
@@ -20,15 +32,17 @@ class conf:
         with open(file) as fp:
             # do stuff with fp
             for cnt, line in enumerate(fp):
-                print(cnt)
-                self.lineCount = cnt
+                #print(cnt)
+                self.lineCount = cnt+1
                 components = re.match(r"^([^#=]*)(?:=([^#]+))?(#.*)?$",line)
-                key=components.group(1)
-                value=components.group(2)
-                comment=components.group(3)
-                if key is not None:# and value is not None:
+                key = components.group(1)
+                value = components.group(2)
+                comment = components.group(3)
+                if key is not None and key.strip():# and value is not None:
+                    if value is None:
+                        value=""
                     self.fields[key.strip()] = value.strip()
-                    self.lines[cnt]=value.strip()
+                    self.lines[cnt] = key.strip()
                 elif (key is None or not key) and (value is None or not value):
                     #print("Ignore line:"+line.strip())
                     pass
@@ -47,16 +61,21 @@ class conf:
         #    self.fields[self.pattern_field]=
         #if self.match_field not in self.fields and "FileAbbrevMatch" in self.fields:
         #    self.match_field = "FileAbrevMatch"
-        self.conf_path = file
+        self.path = file
     ## print conf
     def print(self,indent=""):
         for e in self.fields:
             print(indent+"\t"+e+"\t= "+self.fields[e])
     ## save conf
-    def save(self,outPath=None):
-        if outPath is None:
+    def save(self,out_path=None):
+        if os.path.isdir(out_path):
+            #print("Given dir, add name")
+            out_path=os.path.join(out_path,"lib.conf")
+        if out_path is None or os.path.isfile(out_path):
             print('Conf overwrite disabled for now');
+            #out_path=self.conf_path
             return
+        fp = open(out_path,"w")
         fields=self.fields.copy()
         #for e in self.fields:
         for i in range(0,self.lineCount):
@@ -64,13 +83,19 @@ class conf:
             comment=""
             if i in self.comments:
                 comment=self.comments[i]
-            if i in self.lines:
+            if i in self.lines and not self.lines[i] == "" and self.lines[i] in fields:
                 data=self.lines[i]+"="+fields[self.lines[i]]
                 del fields[self.lines[i]]
             line=data+comment
-            print(line)
+            #print(str(i)+":"+line)
+            #print(line)
+            if not line == "":
+                fp.write(line+"\n")
         for e in fields:
-            line=e+"="+fields[e]
-            print(line)
-        
-    
+            line=""
+            if not e == "":
+                line = e+"="
+            if not fields[e] == "":
+                line=line+fields[e]
+            #print(line)
+            fp.write(line+"\n")
