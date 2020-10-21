a64e213f47484da6502ffe23cfbff5450cbd7bd2
James Cook <jamesjcook@noreplay.github.com>
2020-10-21 13:00:37 -0400

    Finished test case for simplify

--- Testing/SimplifyRatAtlas.py
+++ Testing/SimplifyRatAtlas.py
@@ -1,8 +1,14 @@
 
 
-import sys;sys.path.append(r"h:\code")
+import sys
+import os
+sys.path.append(r"h:\code")
 import ndLibrarySupport
 
-devroot=r"D:\Libraries\010Rat_Brain"
-distRoot=r"D:\Libraries\SimplifiedDistributions\RatAtlas2020"
-ndLibrarySupport.simplify(devRoot, distRoot)
+dev_root = r"D:\Libraries\010Rat_Brain"
+dist_root = r"D:\Libraries\SimplifiedDistributions"
+simplify_job = ndLibrarySupport.simplify(dev_root, dist_root, True)
+simplify_job.show_work_log()
+
+# no-re-set and re-run while working on the code, must start fresh... 
+# i believe that is due to import
