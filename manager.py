916ff6f9c865e3d4f5cee63e7c0664d1d0464678
James Cook <jamesjcook@noreplay.github.com>
2020-10-21 10:40:22 -0400

    Adds click to continue prompt to get user started.
    Also adds inactive logging setup example

--- manager.py
+++ manager.py
@@ -2,6 +2,9 @@
 ## to create base library, and menu object.
 ## Also prompts user to get them started.
 class manager:
+    #module_logger = logging.getLogger('spam_application.auxiliary')
+    #logger = logging.getLogger('ndLibrarySupport')
+    #logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
     def __init__(self,rootDir):
         if not os.path.isdir(rootDir):
             print("Please specify a valid path")
@@ -12,3 +15,4 @@ class manager:
         ## DataPackageMenu will instantiate a controller object that will create the other GUI elements
         self.menu = DataPackageMenu(self.library.getRelevantStrainList())
         slicer.util.messageBox("Click "+self.menu.title+" menu to begin")
+        #self.menu.setProperty("flashing", 0)
