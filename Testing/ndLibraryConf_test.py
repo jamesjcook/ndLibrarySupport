b4bb8366318cc0151931f9eb88243ff878eacbfd
James Cook <jamesjcook@noreplay.github.com>
2020-10-21 13:00:17 -0400

    Added additional test case

--- Testing/ndLibraryConf_test.py
+++ Testing/ndLibraryConf_test.py
@@ -5,3 +5,6 @@ import ndLibrarySupport;
 exec(open(os.path.join(ndLibrarySupport.codePath,"conf.py")).read())
 conf_i = conf(r"D:\Libraries\010Rat_Brain")
 conf_i.print()
+
+conf_b = conf(r"D:\Libraries\010Rat_Brain\v2020-10-16\21Rat","lib.conf",conf_i)
+conf_b.print()
