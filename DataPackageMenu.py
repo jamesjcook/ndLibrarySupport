fe5a492d2ab11cc36f66e54bbb7f0c9d0d21431a
James Cook <jamesjcook@noreplay.github.com>
2020-10-21 10:38:12 -0400

    Renamed fields for ndlibrary to conf
    Added incomplete attempt to use a timer to draw attention to button
    before it is pressed the first time.
    The code is largly inactive. It's currently unclear if it will be
    finished or removed first.

--- DataPackageMenu.py
+++ DataPackageMenu.py
@@ -5,6 +5,8 @@ class DataPackageMenu(qt.QMenu):
     ## ndLibraries is the list of ndLibraries to be displayed in the data package menu
     ## Places menu in the top menu bar (With File, Edit, etc.)
     libDict = dict()
+    firstInteraction = True
+    flashTimer = qt.QTimer()
     def __init__(self, ndLibraries):
         if not isinstance(ndLibraries, list):
             print("Not a list")
@@ -13,6 +15,11 @@ class DataPackageMenu(qt.QMenu):
         mainWindow = slicer.app.activeWindow()
         mainMenuBar = mainWindow.findChild("QMenuBar", "menubar")
         mainMenuBar.addMenu(self)
+        # attempte to add qpushbutton direct fails.
+        #button = qt.QPushButton
+        #button.setMenu(self);
+        #mainMenuBar.addMenu(button)
+        #self.initStyleSheet = self.styleSheet
         self.title = "Data Packages"
         ## Instantiate the controller of the ndLibraryViewer
         self.controller = AtlasController()
@@ -21,10 +28,10 @@ class DataPackageMenu(qt.QMenu):
             if not isinstance(lib, ndLibrary):
                 continue
             name = lib.file_loc
-            if "Strain" in lib.fields and "LibName" in lib.fields:
-                name = lib.fields["Strain"] + " " + lib.fields["LibName"]
-            elif "LibName" in lib.fields:
-                name = lib.fields["LibName"]
+            if "Strain" in lib.conf and "LibName" in lib.conf:
+                name = lib.conf["Strain"] + " " + lib.conf["LibName"]
+            elif "LibName" in lib.conf:
+                name = lib.conf["LibName"]
             menuItem = qt.QAction(name, self)
             self.addAction(menuItem)
             ## Define a function for the menu item to execute when selected
@@ -32,4 +39,36 @@ class DataPackageMenu(qt.QMenu):
             ## storing our lambda's for later so we can manually trigger them.
             self.libDict[name] = lambda instancedLoader, lib=lib: self.controller.setUpLibrary(lib)
             menuItem.triggered.connect(self.libDict[name])
+            #menuItem.triggered.connect(self.flashStop())
             #menuItem.triggered.connect(self.setUpLib)
+        #self.setProperty("flashing", 0)
+        self.flashTimer.timeout.connect(self.flashToggle)
+        # Triggered didnt work to stop the timer, clicked, and entered don't exist, hovered seems to work
+        # Trouble is: it floats in top left corner instead of on menu as expected.
+        #self.triggered.connect(self.flashStop)
+        #self.clicked.connect(self.flashStop)
+        #self.entered.connect(self.flashStop)
+        #self.aboutToShow.connect(self.flashStop)
+        #self.hovered.connect(self.flashStop)
+        #self.flashTimer.start(1500)
+        #self.flashTimer.singleShot(500,self.flashStart)
+    def flashStart(self):
+        self.flashTimer.start(1500)
+    def flashToggle(self):
+        if self.property("flashing") == 0:
+            self.setProperty("flashing", 1)
+            #self.setStyleSheet("QMenu::item {background-color: lightblue; }")
+            self.hide();
+        elif self.property("flashing") == 1:
+            self.setProperty("flashing", 0)
+            #self.setStyleSheet(self.initStyleSheet)
+            #style()->unpolish(this);
+            #style()->polish(this);
+            self.show()
+    def flashStop(self):
+        if self.firstInteraction == True:
+            print("flash stop")
+            self.firstInteraction = False
+            self.flashTimer.stop()
+        #self.flashTimer.timeout.disconnect(self.flashToggle)
+        #self.triggered.disconnect(self.flashStop)
