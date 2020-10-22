11a4f62bb55b897e0b4c7bd611c6d6a4203c19ee
James Cook <jamesjcook@noreplay.github.com>
2020-10-22 11:19:38 -0400

    moving closer to a packaged distribution

--- Testing/ex_slicerrc.py
+++ Testing/ex_slicerrc.py
@@ -1,6 +1,13 @@
 
-# run "rat lib"
-import sys;sys.path.append(r"h:\code");import ndLibrarySupport;ndman=ndLibrarySupport.manager(r"D:\Libraries\010Rat_Brain")
+# run "rat lib" in dev
+#import sys;sys.path.append(r"h:\code");import ndLibrarySupport;ndman=ndLibrarySupport.manager(r"D:\Libraries\010Rat_Brain")
+
+# "rat lib" distribution 
+#dest_lib=r"D:\Libraries\SimplifiedDistributions\DataIndex_Rat_Brain_v2020-10-16"
+dist_root = r"D:\Libraries\SimplifiedDistributions"
+dest_lib = os.path.join(dist_root,"RatBrain_v2020-10-16")
+# run with "dev" code.
+import sys;sys.path.append(r"h:\code");import ndLibrarySupport;ndman=ndLibrarySupport.manager(dest_lib)
 
 # run "brainstem lib"
 #import sys;sys.path.append(r"h:\code");import ndLibrarySupport;ndman=ndLibrarySupport.manager(r"D:\Libraries\040Human_Brainstem")
