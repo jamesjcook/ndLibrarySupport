8fc6ac0849a46e67fe0b97ed99537cf844eee5ab
James Cook <jamesjcook@noreplay.github.com>
2020-10-30 16:37:14 -0400

    Added git update check on start

--- __init__.py
+++ __init__.py
@@ -22,6 +22,44 @@ import slicer
 #class ndLibraryViewer:
 #def __init__(self,rootDir):
 codePath = os.path.dirname(os.path.abspath(__file__))
+
+
+def update_check(codePath,remote_name='origin'):
+    try:
+        import git
+    except ImportError:
+        print(" git unavailable, cannot check for updates of "+codePath)
+        return None
+    try:
+        repo = git.Repo(codePath)
+    except:
+        return None
+    # git fetch
+    try:
+        origin=repo.remote(remote_name)
+    except:
+        return None
+    repo.origin=origin
+    fetch_res=repo.origin.fetch()
+    repo.fetch_res=fetch_res
+    #commits_behind = repo.iter_commits('master..origin/master')
+    local_name=str(repo.heads[0])
+    remote_name=str(origin.refs[0])
+    commits_behind = repo.iter_commits(local_name,remote_name)
+    bcount = sum(1 for c in commits_behind)
+    #commits_ahead = repo.iter_commits('origin/master..master')
+    commits_ahead = repo.iter_commits(remote_name,local_name)
+    acount = sum(1 for c in commits_ahead)
+    if acount > 0:
+        print("Cool! you've made some changes! If you feel like they should be integrated, feel free to fork and issue a pull request!")
+    if bcount > 0:
+        return repo
+    return None
+
+def code_update(repo):
+    # git 
+    return
+
 ## Find code and execute it
 print("ndLibrarySupport: setup code from "+codePath)
 for file in os.listdir(codePath):
@@ -45,3 +83,17 @@ for file in os.listdir(codePath):
     except:
         print("Code start error in file "+file+" from "+codePath)
         #return
+
+try:
+    update_available = update_check(codePath)
+    if update_available is not None:
+        import git
+        print("Code could be updated, installing buttons")
+        ## add a toolbar
+        ## add a button with text "Update ndLibrarySupport"
+except:
+    # our update check should absolutely not prevent running, ever.
+    pass
+
+
+
