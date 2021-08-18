80b07b0fdd3e0f4012c81fafa10c2fdab7d91bb3
harry <harrisonmelman@users.noreply.github.com>
2021-08-18 09:37:37 -0400

    FiducialClickerMenu was added. Creates a menubar shortcut that directly opens the Mouse Mode Menu below it.
    manager.py was edited to run FiducialClickerMenu.
    
    viewNavigatorAnd2DCompare was modified so that it is the default, but does not erase the 4 panel viewNavigatorAnd2DCompare.

---
+++ FiducialClickerMenu.py
@@ -0,0 +1,39 @@
+import slicer
+import qt
+import re 
+import copy
+
+## BAD BEHAVIOR -- children should not place themselves
+## this class auto called on startup (just like Data Package menu)
+
+## FiducialClickerMenu is a subclass of qt.QMenu
+class FiducialClickerMenu(qt.QMenu):
+    def __init__(self): 
+        super(qt.QMenu, self).__init__()
+        mainWindow = slicer.app.activeWindow()
+        mainMenuBar = mainWindow.findChild("QMenuBar", "menubar")
+        #mainMenuBar.addMenu(self)
+        #self.title = r'Fiducial Clicker'
+        
+        
+        ## find the mouse toolbar 
+        a=slicer.util.mainWindow().findChildren("QToolBar")
+        mouse_menu = None
+        for x in a:
+            if re.match(r'.*Mouse.*ToolBar.*', x.name):
+                mouse_menu = x#.actions()
+                
+                
+        ## action is a child of mouse_menu with text="Mouse Interaction"
+        #action=mouse_menu.findChildren("QObject")[2]
+        action=None
+        for x in mouse_menu.findChildren("QObject"):
+            try:
+                if x.text == r"Mouse Interaction":
+                    x.text = "Mouse Contrast Fiducial"
+                    action = x
+                    break
+            except:
+                pass
+        if action:
+            mainMenuBar.addAction(action)
